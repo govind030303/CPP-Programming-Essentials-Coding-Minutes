2a19f35e695916ea38f28e44f0d2de746dc28caa
GOVIND GUPTA <govind030303@gmail.com>
2022-03-24 17:27:13 +0530

    Sorting added

---
+++ kth_smallest_element.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+
+int kthSmallest(int A[], int k, int sizeOfArray) {
+    //implement your code here
+    
+}
+
+void bubble_sort(int Array[], int size) {
+
+    for(int i = 0; i<size; i++) {
+        for(int j = 0; j<size-1; j++) {
+            if(Array[j] > Array[j+1]) {
+                swap(Array[j], Array[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    int Array[size];
+
+    cout<<"Enter the elements: ";
+    for(int i=0; i<size; i++) {
+        cin >> Array[i];
+    }
+
+    bubble_sort(Array, size);
+
+    
+}
